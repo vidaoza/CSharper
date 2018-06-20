b415610034f8ac77cbd8d50d5fbbde483f794e22
vida.opperman <vida.opperman@dariel.co.za>
2018-06-20 09:22:50 +0200

    Initial commit

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        #region Properties
+
+        public int CustomerId { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string HomeAddress { get; set; }
+        public string BusinessAddress { get; set; }
+        public string FullName
+        {
+            get
+            {
+                var fullName = LastName;
+
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(LastName))
+                    {
+                        fullName = $"{fullName}, ";
+                    }
+                    fullName = $"{fullName}{FirstName}";
+                }
+                return fullName;
+            }
+        }
+
+        public static int InstanceCount { get; set; }
+        #endregion
+
+        #region Methods
+
+        public bool Validate()
+        {
+            return !(string.IsNullOrWhiteSpace(LastName) || string.IsNullOrWhiteSpace(EmailAddress));
+        }
+
+        #endregion
+    }
+}
---
+++ ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "FirstName",
+                LastName = "LastName"
+            };
+            var expectedValue = "LastName, FirstName";
+            
+            //Act
+            var actualValue = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameIsEmpty()
+        {
+            //Arrange
+            Customer customer = new Customer
+            {
+                LastName = "LastName"
+            };
+            var expectedValue = "LastName";
+
+            //Act
+            var actualValue = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameIsEmpty()
+        {
+            //Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "FirstName",
+            };
+            var expectedValue = "FirstName";
+
+            //Act
+            var actualValue = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            //Arrange
+            Customer customerOne = new Customer
+            {
+                FirstName = "One",
+            };
+            Customer.InstanceCount++;
+            Customer customerTwo = new Customer
+            {
+                FirstName = "Two",
+            };
+            Customer.InstanceCount++;
+            Customer customerThree = new Customer
+            {
+                FirstName = "Three",
+            };
+            Customer.InstanceCount++;
+            var expectedValue = 3;
+
+            //Act
+            var actualValue = Customer.InstanceCount;
+
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void ValidateValidCustomerTest()
+        {
+            //Arrange
+            Customer validCustomer = new Customer
+            {
+                FirstName = "One",
+                LastName = "LastName",
+                EmailAddress = "one@test.com"
+            };
+            var expectedValue = true;
+
+            //Act
+            var actualValue = validCustomer.Validate();
+
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void ValidateMissingLastNameTest()
+        {
+            //Arrange
+            Customer validCustomer = new Customer
+            {
+                FirstName = "One",
+                EmailAddress = "one@test.com"
+            };
+            var expectedValue = false;
+
+            //Act
+            var actualValue = validCustomer.Validate();
+
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void ValidateMissingEmailAddressTest()
+        {
+            //Arrange
+            Customer validCustomer = new Customer
+            {
+                FirstName = "One",
+                LastName = "LastName",
+            };
+            var expectedValue = false;
+
+            //Act
+            var actualValue = validCustomer.Validate();
+
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
